2ac4b9b40be929305cc117ef6d063bbd815e98aa
Rodrigo Ortega <ortegarod000@gmail.com>
2020-10-12 22:12:26 -0500

    added error handling to migrations

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -8,6 +8,7 @@ contract HelloWorld {
     }
 
     function setMessage(string memory newMessage) public payable {
+        require(msg.value == 1 ether);
         message = newMessage;
     }
 }
--- migrations/2_deploy_HelloWorld.js
+++ migrations/2_deploy_HelloWorld.js
@@ -3,9 +3,11 @@ const HelloWorld = artifacts.require("HelloWorld");
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(HelloWorld).then(function(instance){
       instance.setMessage("Hello Rodrigo!", {value: 1000000, from: accounts[0]}).then(function(){
-        instance.getMessage().then(function(message){
-          console.log("Current message: " + message);
-        });
+        console.log("success");
+      }).catch(function(err){
+        console.log("Error: " + err);
       });
+  }).catch(function(err){
+    console.log("Deploy failed " + err);
   });
 };
